a790a6f77027a13b09ee72389b418c8f5a6e45d1
Reid Baker <hamiltonbaker@yahoo.com>
2012-09-16 19:20:38 -0400

    doesnt error, but not graphing how I would like.

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function tempBarGraph(weatherData, w, h){
 
 	var curve = d3.svg.line()
 	.x(function(d,i){
-	    console.log(i);
+//	    console.log(i);
 	    return i * (w/temps.length); })
 	.y(function(d){
 	    return h-yScale(d); })
@@ -51,11 +51,11 @@ function tempBarGraph(weatherData, w, h){
 	.attr("fill", "none");
 }
 
-function graph(x, y){
-    var w = 700;
-    var h = 100;
+function tide_graph(w, h, tide){
     var barPadding = 1;
-    var test = [20,30,20,50,60];
+    var test = tide.tideSummary
+
+//    console.log(tide.tideSummary)
 
     var svg = d3.select("#graph")
 	.append("svg")
@@ -63,17 +63,43 @@ function graph(x, y){
 	.attr("width", w)
 	.attr("height", h);
 
+// should have to use: parseFloat(tideH.slice(0,tideH.indexOf(' '))) but magic
     var curve = d3.svg.line()
 	.x(function(d,i){
-	    console.log(i);
+//	    console.log(d);
 	    return i*20; })
-	.y(function(d){
-	    return d; })
+	.y(function(d, i){
+	    var val = parseFloat(d.data.height);
+	    if(isNaN(val)){
+	    }
+	    else{
+		console.log(val);
+		console.log(i);
+	    }
+	    return val
+	})
 	.interpolate("monotone");
 
     svg.append("svg:path")
-	.attr("d",curve(test))
+	.attr("d",d3.svg.symbol())
 	.attr("stroke", "black")
 	.attr("fill", "none");
+}
+
+function showTideData(w, h){
+    var data;
+    var city = "CA/San_Francisco"
+    var url = "http://api.wunderground.com/api/9da96fc7939df769/conditions/tide/hourly10day/q/"+city+ ".json";
+
+    $.ajax({
+
+        url: url,
+
+        dataType: "jsonp",
+
+        success: function(tideData) {
+	    tide_graph(w, h, tideData.tide);
+	}
+    });
 }
 
