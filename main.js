cb7ee8e97f5931341d01a1aebb304065be72a5dd
Reid Baker <hamiltonbaker@yahoo.com>
2012-09-18 19:40:32 -0400

    fixed spacing

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function tempBarGraph(weatherData, w, h){
     var temps = [];
     //console.log(hours[0].temp.english);
     for(var i = 0; i<hours.length; i++){
-    temps[i] = hours[i].temp.english;
-    //console.log(temps[i]);
+	temps[i] = hours[i].temp.english;
+	//console.log(temps[i]);
     }
 
     var barPadding = 1;
@@ -14,76 +14,76 @@ function tempBarGraph(weatherData, w, h){
                      .range([0, h]);
 
     var svg = d3.select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+	.append("svg")
+	.attr("width", w)
+	.attr("height", h);
     
     svg.selectAll("rect")
-    .data(temps)
-    .enter()
-    .append("rect")
-    .attr("x", function(d,i){
-        return i * (w/temps.length);
-    })
-    .attr("y", function(d){
-        return h-yScale(d);
-    })
-    .attr("width", w/temps.length - barPadding)
-    .attr("height", function(d){
-        return yScale(d);
-    })
-    .attr("fill", function(d){
-        return "rgb(0,127,255)";
-    });
+	.data(temps)
+	.enter()
+	.append("rect")
+	.attr("x", function(d,i){
+            return i * (w/temps.length);
+	})
+	.attr("y", function(d){
+            return h-yScale(d);
+	})
+	.attr("width", w/temps.length - barPadding)
+	.attr("height", function(d){
+            return yScale(d);
+	})
+	.attr("fill", function(d){
+            return "rgb(0,127,255)";
+	});
 
     var curve = d3.svg.line()
-    .x(function(d,i){
+	.x(function(d,i){
 //        console.log(i);
-        return i * (w/temps.length); })
-    .y(function(d){
-        return h-yScale(d); })
-    .interpolate("basis");
+            return i * (w/temps.length); })
+	.y(function(d){
+            return h-yScale(d); })
+	.interpolate("basis");
 
     svg.append("svg:path")
-    .attr("d",curve(temps))
-    .attr("stroke-width", "3")
-    .attr("stroke", "rgba(0,0,0,0.5)")
-    .attr("fill", "none");
+	.attr("d",curve(temps))
+	.attr("stroke-width", "3")
+	.attr("stroke", "rgba(0,0,0,0.5)")
+	.attr("fill", "none");
 }
 
 function tide_graph(w, h, tide){
     var barPadding = 1;
     var test = tide.tideSummary;
 
-//    console.log(tide.tideSummary)
+    //    console.log(tide.tideSummary)
 
     var svg = d3.select("#graph")
-    .append("svg")
-    .data(test)
-    .attr("width", w)
-    .attr("height", h);
+	.append("svg")
+	.data(test)
+	.attr("width", w)
+	.attr("height", h);
 
 // should have to use: parseFloat(tideH.slice(0,tideH.indexOf(' '))) but magic
     var curve = d3.svg.line()
-    .x(function(d,i){
-//        console.log(d);
-        return i*20; })
-    .y(function(d, i){
-        var val = parseFloat(d.data.height);
-        if(isNaN(val)){
-        }
-        else{
-        console.log(val);
-        console.log(i);
-        }
-        return val;
-    })
-    .interpolate("monotone");
+	.x(function(d,i){
+	    //        console.log(d);
+            return i*20; })
+	.y(function(d, i){
+            var val = parseFloat(d.data.height);
+            if(isNaN(val)){
+            }
+            else{
+		console.log(val);
+		console.log(i);
+            }
+            return val;
+	})
+	.interpolate("monotone");
 
     svg.append("svg:path")
-    .attr("d",d3.svg.symbol())
-    .attr("stroke", "black")
-    .attr("fill", "none");
+	.attr("d",d3.svg.symbol())
+	.attr("stroke", "black")
+	.attr("fill", "none");
 }
 
 function showTideData(w, h){
@@ -92,11 +92,8 @@ function showTideData(w, h){
     var url = "http://api.wunderground.com/api/9da96fc7939df769/conditions/tide/hourly10day/q/"+city+ ".json";
 
     $.ajax({
-
         url: url,
-
         dataType: "jsonp",
-
         success: function(tideData) {
             tide_graph(w, h, tideData.tide);
         }
