72d2fa9c13ac529fc9df3714a46705d2104287a2
Reid Baker <hamiltonbaker@yahoo.com>
2012-09-18 19:23:07 -0400

    no longer mix tabs and spaces

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function tempBarGraph(weatherData, w, h){
     var temps = new Array();
     //console.log(hours[0].temp.english);
     for(var i = 0; i<hours.length; i++){
-	temps[i] = hours[i].temp.english;
-	//console.log(temps[i]);
+    temps[i] = hours[i].temp.english;
+    //console.log(temps[i]);
     }
 
     var barPadding = 1;
@@ -14,41 +14,41 @@ function tempBarGraph(weatherData, w, h){
                      .range([0, h]);
 
     var svg = d3.select("body")
-	.append("svg")
-	.attr("width", w)
-	.attr("height", h);
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
     
     svg.selectAll("rect")
-	.data(temps)
-	.enter()
-	.append("rect")
-	.attr("x", function(d,i){
-	    return i * (w/temps.length);
-	})
-	.attr("y", function(d){
-	    return h-yScale(d);
-	})
-	.attr("width", w/temps.length - barPadding)
-	.attr("height", function(d){
-	    return yScale(d);
-	})
-	.attr("fill", function(d){
-	    return "rgb(0,127,255)";
-	});
+    .data(temps)
+    .enter()
+    .append("rect")
+    .attr("x", function(d,i){
+        return i * (w/temps.length);
+    })
+    .attr("y", function(d){
+        return h-yScale(d);
+    })
+    .attr("width", w/temps.length - barPadding)
+    .attr("height", function(d){
+        return yScale(d);
+    })
+    .attr("fill", function(d){
+        return "rgb(0,127,255)";
+    });
 
-	var curve = d3.svg.line()
-	.x(function(d,i){
-//	    console.log(i);
-	    return i * (w/temps.length); })
-	.y(function(d){
-	    return h-yScale(d); })
-	.interpolate("basis");
+    var curve = d3.svg.line()
+    .x(function(d,i){
+//        console.log(i);
+        return i * (w/temps.length); })
+    .y(function(d){
+        return h-yScale(d); })
+    .interpolate("basis");
 
-	svg.append("svg:path")
-	.attr("d",curve(temps))
-	.attr("stroke-width", "3")
-	.attr("stroke", "rgba(0,0,0,0.5)")
-	.attr("fill", "none");
+    svg.append("svg:path")
+    .attr("d",curve(temps))
+    .attr("stroke-width", "3")
+    .attr("stroke", "rgba(0,0,0,0.5)")
+    .attr("fill", "none");
 }
 
 function tide_graph(w, h, tide){
@@ -58,32 +58,32 @@ function tide_graph(w, h, tide){
 //    console.log(tide.tideSummary)
 
     var svg = d3.select("#graph")
-	.append("svg")
-	.data(test)
-	.attr("width", w)
-	.attr("height", h);
+    .append("svg")
+    .data(test)
+    .attr("width", w)
+    .attr("height", h);
 
 // should have to use: parseFloat(tideH.slice(0,tideH.indexOf(' '))) but magic
     var curve = d3.svg.line()
-	.x(function(d,i){
-//	    console.log(d);
-	    return i*20; })
-	.y(function(d, i){
-	    var val = parseFloat(d.data.height);
-	    if(isNaN(val)){
-	    }
-	    else{
-		console.log(val);
-		console.log(i);
-	    }
-	    return val
-	})
-	.interpolate("monotone");
+    .x(function(d,i){
+//        console.log(d);
+        return i*20; })
+    .y(function(d, i){
+        var val = parseFloat(d.data.height);
+        if(isNaN(val)){
+        }
+        else{
+        console.log(val);
+        console.log(i);
+        }
+        return val
+    })
+    .interpolate("monotone");
 
     svg.append("svg:path")
-	.attr("d",d3.svg.symbol())
-	.attr("stroke", "black")
-	.attr("fill", "none");
+    .attr("d",d3.svg.symbol())
+    .attr("stroke", "black")
+    .attr("fill", "none");
 }
 
 function showTideData(w, h){
@@ -98,8 +98,8 @@ function showTideData(w, h){
         dataType: "jsonp",
 
         success: function(tideData) {
-	    tide_graph(w, h, tideData.tide);
-	}
+            tide_graph(w, h, tideData.tide);
+        }
     });
 }
 
