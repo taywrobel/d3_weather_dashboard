e8a4aa8b0d691e15ca96351c61603c0efa752308
Reid Baker <hamiltonbaker@yahoo.com>
2012-09-19 00:46:56 -0400

    now there is an axis function and tide circles

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function makeScale(dMin,dMax,rMin,rMax){
-	return d3.scale.linear().domain([dMin,dMax]).range([rMin,rMax]);
+    return d3.scale.linear().domain([dMin,dMax]).range([rMin,rMax]);
 }
 
 function tempBarGraph(weatherData, w, h){
@@ -40,14 +40,14 @@ function tempBarGraph(weatherData, w, h){
         .attr("fill", function(d){
             return "rgb(0,127,255)";
         })
-		.on("mouseover", function(){
-			var bar = d3.select(this);
-			bar.attr("fill","0");
-		})
-		.on("mouseout", function(){
-			var bar = d3.select(this);
-			bar.attr("fill","rgb(0,127,255)")
-		});
+        .on("mouseover", function(){
+            var bar = d3.select(this);
+            bar.attr("fill","0");
+        })
+        .on("mouseout", function(){
+            var bar = d3.select(this);
+            bar.attr("fill","rgb(0,127,255)");
+        });
 
     var curve = d3.svg.line()
         .x(function(d,i){
@@ -64,10 +64,9 @@ function tempBarGraph(weatherData, w, h){
         .attr("fill", "none");
 }
 
-function tide_graph(w, h, tide){
+function tide_graph(svg, w, h, tide){
     var barPadding = 1;
     var tide_heights = [];
-
     var next_tide = 0;
     for(i = 0; i < tide.tideSummary.length; i++){
         var tide_height = tide.tideSummary[i].data.height;
@@ -80,46 +79,73 @@ function tide_graph(w, h, tide){
             //console.log(tide_height);
         }
     }
-    console.log(tide_heights);
+    //console.log(tide_heights);
 
-    var tide_scale = makeScale(d3.min(tide_heights), d3.max(tide_heights), 0, h);
-
-    var svg = d3.select("#graph")
-        .append("svg")
-        .data(tide_heights)
-        .attr("width", w)
-        .attr("height", h);
-
-    var curve = d3.svg.line()
-        .x(function(d,i){
-            //console.log(d);
-            return i*20; })
-        .y(function(d, i){
-            return tide_scale(d);
-        })
-        .interpolate("monotone");
-
-    svg.append("svg:path")
-        .attr("d",curve(tide_heights))
-        .attr("stroke", "black")
-        .attr("fill", "none");
+    svg.data(tide_heights);
+    var tide_scale_circle = makeScale(d3.min(tide_heights), d3.max(tide_heights), 5, 25);
 
     svg.selectAll("circle")
         .data(tide_heights)
         .enter()
-        .append("rect")
-        .attr("x", function(d,i){
-            return i * 20;
+        .append("circle")
+        .attr("cx", function(d,i){
+            return (i+1) * 20;
         })
-        .attr("y", function(d){
+        .attr("cy", function(d){
             return h/2;
         })
         .attr("r", function(d){
-            tide_scale(d);
+            return tide_scale_circle(d);
         })
-        .attr("fill", "steelblue");
+        .attr("fill", "blue")
+        .attr("opacity", "0.5");
 
 }
+
+function draw_axis(svg, x, y, width, height, x_label, y_label){
+
+    //x axis
+    svg.append("svg:line")
+        .attr("x1",x)
+        .attr("y1",y)
+        .attr("x2",x + width)
+        .attr("y2",y)
+        .attr("stroke", "black");
+
+    //tic marks on x
+    var tic_jump = 10;
+    var tic_height = 10;
+    console.log(width);
+    for(var i = x; i<(x+width); i = i + tic_jump){
+        svg.append("svg:line")
+            .attr("x1", i)
+            .attr("y1",y - tic_height/2)
+            .attr("x2", i)
+            .attr("y2", y + tic_height/2)
+            .attr("stroke", "black");
+    }
+
+/*    //y axis ends
+    svg.append("svg:line")
+        .attr("x1",0+xMargin)
+        .attr("y1",0+yMargin)
+        .attr("x2",0+xMargin)
+        .attr("y2",h+yMargin)
+        .attr("stroke", "black");
+    
+    //vertical labels
+    var labels = [100,0,100];
+    var yLoc = [5, h/2+5, h+5];
+    for(var i = 0; i<labels.length; i++){
+        svg.append("svg:text")
+            .attr("x",0+xMargin-2)
+            .attr("y",yLoc[i]+yMargin)
+            .text (labels[i] + "%")
+            .attr("text-anchor", "end")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "11px");
+    }
+*/}
 
 function showTideData(w, h){
     var data;
@@ -130,7 +156,13 @@ function showTideData(w, h){
         url: url,
         dataType: "jsonp",
         success: function(tideData) {
-            tide_graph(w, h, tideData.tide);
+            var svg = d3.select("#graph")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+            tide_graph(svg, w, h, tideData.tide);
+            draw_axis(svg, 1, h/2, w, h, "tide height", "tide_time");
         }
     });
 }
